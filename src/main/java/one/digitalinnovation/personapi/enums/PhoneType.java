3df3986118bc7e9a74ed79a7e686a42215ea9459
Micaely Gusmão <devmicaelyg@gmail.com>
2020-09-19 10:54:06 -0300

    Criação e mapeamento das entidades

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMERCIAL("Comercial");
+
+    private final String description;
+}
