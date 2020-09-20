6865079851a9d4fd52d17aaa8ae47f792717ad2f
Micaely Gusmão <devmicaelyg@gmail.com>
2020-09-19 23:24:27 -0300

    Criação do mapper

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -29,8 +29,8 @@ public class Person {
     @Column(nullable = false, unique = true)
     private String cpf;
 
-    private LocalDate bithDate;
+    private LocalDate birthDate;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
-    private List<Phone> phone;
+    private List<Phone> phones;
 }
